3be7a29dad13a086e2bb7fc031a3fe916948812c
Gopalakrishnan <rago@softwareag.com>
2021-02-06 07:37:04 +0530

    Initial commit

---
+++ src/test/java/com/example/springsecurityfacecbooklogin/SpringsecurityFacecbookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityfacecbooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacecbookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
